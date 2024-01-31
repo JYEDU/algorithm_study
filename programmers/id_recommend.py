f29ad2a7acd0141674816a92c6e0c4517904f72b
JYEDU <z940405@naver.com>
2024-01-31 22:54:56 +0900

    2024.01.31 commit

---
+++ programmers/id_recommend.py
@@ -0,0 +1,24 @@
+import re
+
+def solution(new_id):
+    '''
+    new id recommendation
+    link : https://school.programmers.co.kr/learn/courses/30/lessons/72410#
+    
+    3<=length of id <= 15
+    The ID can only contain lowercase letters, numbers, minus (-), underscore (_), and period (.) characters.
+    However, the period (.) cannot be used at the beginning and end, and cannot be used consecutively.
+    '''
+    out = new_id.lower()
+    out = re.sub(r'[^a-z0-9-_.]', '', out)
+    out = re.sub(r'\.{2,}', '.', out)
+    out = re.sub(r'^\.|\.$', '', out)
+    if not out:
+        out = 'a'
+    if len(out)>=16:
+        out = out[:15]    
+        if out[-1]=='.':
+            out = out[:-1]
+    if len(out)<=2:
+        out=out+out[-1]*(3-len(out))
+    return out
---
+++ programmers/menu_renewal.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    '''
+    Menu Renewal
+    link : https://school.programmers.co.kr/learn/courses/30/lessons/72411
+    '''
+    answer = []
+
+    for course_size in course:
+        course_candidates = []
+        for order in orders:
+            order_combinations = combinations(sorted(order), course_size)
+            course_candidates.extend(order_combinations)
+
+        if not course_candidates:
+            continue
+
+        counter = Counter(course_candidates)
+        max_count = max(counter.values())
+
+        if max_count >= 2:
+            answer += [''.join(course) for course, count in counter.items() if count == max_count]
+
+    return sorted(answer)
