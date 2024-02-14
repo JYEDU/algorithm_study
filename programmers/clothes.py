9e0460a193f9832d88f92b91ad7a8a3dd53727aa
DESKTOP-3T9U3KO\tb21 <z940405@naver.com>
2024-02-14 23:26:49 +0900

    2024.02.14 commit

---
+++ programmers/clothes.py
@@ -0,0 +1,16 @@
+'''
+의상
+link: https://school.programmers.co.kr/learn/courses/30/lessons/42578
+'''
+from collections import Counter
+from functools import reduce
+
+def solution(clothes):
+    # 각 종류별 의상 개수를 카운트
+    counter = Counter([kind for name, kind in clothes])
+
+    # 각 종류별 의상 개수에 1을 더하고 모두 곱함
+    answer = reduce(lambda x, y: x * (y + 1), counter.values(), 1)
+
+    # 아무것도 입지 않는 경우를 제외함
+    return answer - 1
---
+++ programmers/non_complete_runner.py
@@ -0,0 +1,12 @@
+'''
+완주하지 못한 선수
+link : https://school.programmers.co.kr/learn/courses/30/lessons/42576
+'''
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
